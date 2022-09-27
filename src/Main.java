da4261964a1719ccb4c99061e78b2716ca262db6
Garik-create <pfdfkbybujhm@yandex.ru>
2022-09-27 12:56:07 +0300

    Выполнил д.з.2

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,28 @@
+import transport.Car;
+
 public class Main {
     public static void main(String[] args) {
-        Car ladaGrande = new Car(null,null,1.7,"Жёлтый",2015,
-                null);
+        Car ladaGrande = new Car(null, null, 1.7, "Жёлтый", 2015,
+                null, "Механика", "Седан", "е922ыв333", 4,
+                "Зима");
 
-        Car audiA850LTdiQuattro = new Car("Audi","A8 50 L TDI quattro",0,"Чёрный",
-                2020,"Германия");
+        Car audiA850LTdiQuattro = new Car("Audi", "A8 50 L TDI quattro", 0, "Чёрный",
+                2020, "Германия", "автомат", "Хэтчбек", "е345нн333",
+                5, "лето");
 
-        Car bmwZ8 = new Car("BMW","Z8",3.0,null,2021,
+        Car bmwZ8 = new Car("BMW", "Z8", 3.0, null, 2021,
 
-                "Германия");
-        Car kiaSportage = new Car("Kia","4 поколение", 2.4,"Красный",0,
-                "Южная Корея");
+                "Германия", "робот", "универсал", "у999ке888",
+                3, "лто");
+        Car kiaSportage = new Car("Kia", "4 поколение", 2.4, "Красный", 0,
+                "Южная Корея", "полуавтомат", "лифтбек", "а444пр666",
+                8, "стужа");
 
 
-        Car hyundaiAvante = new Car("Hyundai","Avante",1.6,"Оранжевый",2016,
-                "Южная Корея");
+        Car hyundaiAvante = new Car("Hyundai", "Avante", 1.6, "Оранжевый",
+                2016,
+                "Южная Корея", "вариатор", "минивэн", "у432ке456",
+                15, "Лето");
 
 
         ladaGrande.printData();
@@ -22,6 +30,23 @@ public class Main {
         bmwZ8.printData();
         kiaSportage.printData();
         hyundaiAvante.printData();
+        System.out.println();
+        System.out.println();
+
+
+        int temperature = 7;
+
+        ladaGrande.changeWheels(temperature, ladaGrande.wheels);
+        audiA850LTdiQuattro.changeWheels(temperature, audiA850LTdiQuattro.wheels);
+        bmwZ8.changeWheels(temperature, bmwZ8.wheels);
+        kiaSportage.changeWheels(temperature, kiaSportage.wheels);
+        hyundaiAvante.changeWheels(temperature, hyundaiAvante.wheels);
+
+        ladaGrande.isNumberTrue(ladaGrande.number);
+        audiA850LTdiQuattro.isNumberTrue(audiA850LTdiQuattro.number);
+        bmwZ8.isNumberTrue(bmwZ8.number);
+        kiaSportage.isNumberTrue(kiaSportage.number);
+        hyundaiAvante.isNumberTrue(hyundaiAvante.number);
 
     }
 }
---
+++ src/transport/Car.java
@@ -0,0 +1,190 @@
+package transport;
+
+public class Car {
+    private String brand;
+    private String model;
+    public double engineVolume;
+    public String color;
+    private int productionYear;
+    private String productionCountry;
+    public String speedBox;
+    private String carType;
+    public String number;
+    private int seatsAmount;
+    public String wheels;
+
+
+    public Car(String brand,
+               String model,
+               double engineVolume,
+               String color,
+               int productionYear,
+               String productionCountry,
+               String speedBox,
+               String carType,
+               String number,
+               int seatsAmount,
+               String wheels) {
+        if (brand != null) {
+            this.brand = brand;
+        } else {
+            this.brand = "default";
+        }
+        if (model != null) {
+            this.model = model;
+        } else {
+            this.model = "default";
+        }
+        if (engineVolume != 0) {
+            this.engineVolume = engineVolume;
+        } else {
+            this.engineVolume = 1.5;
+        }
+        if (color != null) {
+            this.color = color;
+        } else {
+            this.color = "белый";
+        }
+        if (productionYear != 0) {
+            this.productionYear = productionYear;
+        } else {
+            this.productionYear = 2000;
+        }
+        if (productionCountry != null) {
+            this.productionCountry = productionCountry;
+        } else {
+            this.productionCountry = "default";
+        }
+        if (speedBox != null && !speedBox.isBlank() && !speedBox.isEmpty()) {
+            this.speedBox = speedBox;
+        } else {
+            this.speedBox = "Механика";
+        }
+        if (carType != null && !carType.isEmpty() && !carType.isBlank()) {
+            this.carType = carType;
+        } else {
+            this.carType = "Седан";
+        }
+        if (number != null && !number.isBlank() && !number.isEmpty()) {
+            this.number = number;
+        } else {
+            this.number = "x000xx000";
+        }
+        if (seatsAmount >= 1) {
+            this.seatsAmount = seatsAmount;
+        } else {
+            this.seatsAmount = 0;
+        }
+        if (wheels.equalsIgnoreCase("зима") || wheels.equalsIgnoreCase("лето")) {
+            this.wheels = wheels;
+        } else {
+            this.wheels = "Не указано";
+        }
+
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getProductionYear() {
+        return productionYear;
+    }
+
+    public String getProductionCountry() {
+        return productionCountry;
+    }
+
+    public String getCarType() {
+        return carType;
+    }
+
+    public int getSeatsAmount() {
+        return seatsAmount;
+    }
+
+    public double getEngineVolume() {
+        return engineVolume;
+    }
+
+    public void setEngineVolume(double engineVolume) {
+        this.engineVolume = engineVolume;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getSpeedBox() {
+        return speedBox;
+    }
+
+    public void setSpeedBox(String speedBox) {
+        this.speedBox = speedBox;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getWheels() {
+        return wheels;
+    }
+
+    public void setWheels(String wheels) {
+        this.wheels = wheels;
+    }
+
+    public void printData() {
+        System.out.printf("\n Автомобиль %s %s, объём двигателя %.1f литра, цвет %s, сборка %d, страна %s, коробка передач: %s, кузов: %s, номер: %s, количество мест: %d, резина: %s",
+                brand, model, engineVolume, color, productionYear, productionCountry, speedBox,
+                carType, number, seatsAmount, wheels);
+    }
+
+    public void changeWheels(int temperature, String wheels) {
+        if (temperature < 8 && this.wheels.equalsIgnoreCase("лето")) {
+            System.out.println("Смени резину на зимнюю!");
+        } else if (temperature >= 8 && this.wheels.equalsIgnoreCase("зима")) {
+            System.out.println("Смени резину на летнюю!");
+        } else {
+            System.out.println("Резину менять не надо.");
+        }
+    }
+
+    public void isNumberTrue(String number) {
+        if (number.length() != 9 || number.charAt(0) < 1072 || number.charAt(0) > 1103) {
+            System.out.println("\nНеверно указан номер");
+            return;
+        }
+        for (int i = 1; i <= 3; i++) {
+            if ((int) number.charAt(i) < 48 || number.charAt(i) > 57) {
+                System.out.println("\nНеверно указан номер");
+                return;
+            }
+        }
+        for (int i = 4; i <= 5; i++) {
+            if ((int) number.charAt(i) < 1072 || number.charAt(i) > 1103) {
+                System.out.println("\nНеверно указан номер");
+                return;
+            }
+        }
+        for (int i = 6; i <= 8; i++) {
+            if ((int) number.charAt(i) < 48 || number.charAt(i) > 57) {
+                System.out.println("\nНеверно указан номер");
+                return;
+            }
+        }
+    }
+}
